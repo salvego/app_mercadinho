962a8025f3aa51056e9398f5adecb1e976b9099a
salvego <fael_ssantos@yahoo.com.br>
2022-01-24 08:04:03 -0300

    Update api_get_category_list.dart

--- lib/src/api/product/api_get_category_list.dart
+++ lib/src/api/product/api_get_category_list.dart
@@ -3,15 +3,17 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 void getCategoryList() async {
 
-  int i = 0;
+  List<String> category = [];
 
   final ParseCloudFunction function = ParseCloudFunction('get-category-list');
   final ParseResponse parseResponse = await function.execute();
 
-  List<String> category = [];
+  for(var item in parseResponse.result ) {
 
-  category.addAll(parseResponse.result as List<String>);
+    category.add(item['title']);
 
-  print(category);
+  }
+
+  return print(category);
 
 }
