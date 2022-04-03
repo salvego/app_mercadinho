c56ab7bf5007db11ef9d2aa6138a59df902c1957
salvego <56658747+salvego@users.noreply.github.com>
2022-04-03 19:47:24 -0300

    Update key_back4app.dart

--- lib/src/config/key_back4app.dart
+++ lib/src/config/key_back4app.dart
@@ -1,3 +1,3 @@
-const keyApplicationId = 'RhUzJVzwjhjlor0BqO0lwexoXv7FwdRr1QavFA8F';
-const keyClientKey = '02mgdO9yN8Voi1o29xCOvXyZ04gDFamnqPEy9KTC';
+const keyApplicationId = '';
+const keyClientKey = '';
 const keyParseServerUrl = 'https://parseapi.back4app.com';
